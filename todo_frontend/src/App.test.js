e1b3228c9330937d640b7cf845ba505638d8373c
babu <babu.k@kavia.ai>
2026-02-13 10:12:18 +0000

    Checkpoint 2 - cg3dc8867c

--- todo_frontend/src/App.test.js
+++ todo_frontend/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders the Todos header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByRole("heading", { name: /your tasks/i });
+  expect(title).toBeInTheDocument();
 });
